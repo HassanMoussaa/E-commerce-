83ee337501824a1ed031c54d1a79b719051fc625
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-29 21:52:10 +0300

    added first js code for sign up page

---
+++ scripts/sign_up.js
@@ -0,0 +1,72 @@
+function isValidEmail(email) {
+  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return email_pattern.test(email);
+}
+
+function isValidPassword(password) {
+  const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  return password_pattern.test(password);
+}
+
+function isValidName(name) {
+  const name_pattern = /^[A-Za-z\s]+$/;
+  return name_pattern.test(name);
+}
+
+function move() {
+  window.location.href = "../pages/signin.html";
+}
+
+const btn = document.querySelector(".btn-signin"); 
+const err = document.getElementById("error");
+
+btn.addEventListener("click", () => {
+  const fullname = document.getElementById("fullname").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const userType = document.getElementById("user-type").value;
+  
+  
+  err.innerText = "";
+
+  if (!isValidEmail(email)) {
+    err.innerText = "Email should be similar to username@example.com";
+  } else if (!isValidPassword(password)) {
+    err.innerText =
+      "Password should have at least one digit, one lowercase letter, one uppercase letter, and be at least 8 characters long";
+  } else if (!isValidName(fullname)) {
+    err.innerText = "Full name should contain only alphabetic characters";
+  } else if (userType === "") {
+    err.innerText = "Choose a user type";
+  } else {
+
+    //  proceed with the POST request
+    console.log(userType);
+    err.innerText = "";
+
+    
+    const userData = {
+      name: fullname,
+      email: email,
+      password: password,
+      user_types_id: userType,
+    };
+
+      axios
+      .post("http://127.0.0.1:8000/api/register", userData)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.message === "User created successfully") {
+          err.setAttribute("class", "success");
+          err.innerText = "Created successfully!";
+          setTimeout(move, 2000);
+        } else {
+          err.innerText = "An error occurred. Please try again later.";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        err.innerText = "An error occurred. Please try again later.";
+      });
+  }
+});
