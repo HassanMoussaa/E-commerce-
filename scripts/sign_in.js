a020cc2cc3ebca3c309cfaab2ac8a80f39495d79
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-29 20:05:28 +0300

    adjusted js file to submit event listener

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -1,15 +1,17 @@
-const btn = document.getElementById("btn-signin");
-let err = document.getElementById("error");
+const form = document.querySelector("form");
+const err = document.getElementById("error");
 
-btn.addEventListener("click", () => {
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent default form submission behavior
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  data = new FormData();
+  const data = new FormData();
   data.append("email", email);
   data.append("password", password);
 
-  axios.post(`${this.base_url}sign_in.php`, data)
+  axios.post("http://127.0.0.1:8000/api/register", data)
     .then((response) => {
       console.log(response.data);
       if (response.data.message === "Unauthorized") {
@@ -17,23 +19,16 @@ btn.addEventListener("click", () => {
       } else {
         err.innerText = "";
 
-        
         const userTypeId = response.data.user.user_type_id;
 
-      
         window.localStorage.setItem('id', response.data.user.id);
-     
         window.localStorage.setItem('user_type_id', userTypeId);
 
-        
         if (userTypeId === 1) {
-          // User is an admin, redirect to dashboard_admin page
           window.location.href = "../pages/dashboard_admin.html";
         } else if (userTypeId === 2) {
-          // User is a regular user, redirect to index.html
           window.location.href = "../pages/index.html";
         } else {
-          
           console.error("Invalid user_type_id:", userTypeId);
         }
       }
