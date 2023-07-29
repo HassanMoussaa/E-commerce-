89dc77d8cfa99896fb7954f5046c70a3e2727787
Hassan Moussa <hassanmoussa61@gmail.com>
2023-07-29 19:38:05 +0300

    fixed part in sign taking user id into consideration

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -6,22 +6,36 @@ btn.addEventListener("click", () => {
   const password = document.getElementById("password").value;
 
   data = new FormData();
-  data.append("email", email); // Change "input" to "email" to match the server-side validation
+  data.append("email", email);
   data.append("password", password);
 
   axios.post(`${this.base_url}sign_in.php`, data)
     .then((response) => {
       console.log(response.data);
-      if (response.data.message === "Unauthorized" ) {
+      if (response.data.message === "Unauthorized") {
         err.innerText = "Incorrect credentials";
       } else {
         err.innerText = "";
+
+        
+        const userTypeId = response.data.user.user_type_id;
+
+      
         window.localStorage.setItem('id', response.data.user.id);
-        window.localStorage.setItem('user_type_id', response.data.user.user_type_id); 
-        window.location.href = "../pages/index.html";
-          const token = response.data.authorization.token;
-       
-        window.localStorage.setItem('token', token);
+     
+        window.localStorage.setItem('user_type_id', userTypeId);
+
+        
+        if (userTypeId === 1) {
+          // User is an admin, redirect to dashboard_admin page
+          window.location.href = "../pages/dashboard_admin.html";
+        } else if (userTypeId === 2) {
+          // User is a regular user, redirect to index.html
+          window.location.href = "../pages/index.html";
+        } else {
+          
+          console.error("Invalid user_type_id:", userTypeId);
+        }
       }
     })
     .catch((error) => {
